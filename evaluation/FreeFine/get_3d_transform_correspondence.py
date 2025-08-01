cd2ccda5efa05f6f206ced898b6f181da5bb9457
CIawevy <519511374@qq.com>
2025-08-02 04:59:36 +0800

    updates for evaluation

--- evaluation/FreeFine/get_3d_transform_correspondence.py
+++ evaluation/FreeFine/get_3d_transform_correspondence.py
@@ -1,8 +1,8 @@
 from vis_utils import  temp_view,temp_view_img,save_img,save_mask
 import sys
 # from xml.dom.minidom import Notation
-sys.path.append("/data/Hszhu/FreeFine/evaluation/GeoDiffuser/GeoDiffuser")
-sys.path.append("/data/Hszhu/FreeFine/evaluation/GeoDiffuser")
+sys.path.append("/data/Hszhu/FreeFine/evaluation/GeoDiffuser/GeoDiffuser") #replace with your own
+sys.path.append("/data/Hszhu/FreeFine/evaluation/GeoDiffuser") #replace with your own
 from utils.ui_utils2 import get_transformed_mask, get_depth, get_mask, get_edited_image
 from PIL import Image
 import numpy as np
--- evaluation/FreeFine/run_script_3D.sh
+++ evaluation/FreeFine/run_script_3D.sh
@@ -9,12 +9,12 @@ FREE_PORT=$(python -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(
 #For 3D evaluation
 
 #Step 1: run batch inference to generate background
-torchrun --nproc_per_node=8 --master-port $FREE_PORT freefine_batch_infer_bggen_3d.py  #replace with your own path in the file
+# torchrun --nproc_per_node=8 --master-port $FREE_PORT freefine_batch_infer_bggen_3d.py  #replace with your own path in the file
 
 #Step2 run depth-based 3D transform where you got corase_edit image and 3d_correspondence for MD metric calculation
-conda deactivate 
-conda activate GeoDiffuser
-python get_3d_transform_correspondence.py
+# conda deactivate 
+# conda activate GeoDiffuser
+# python get_3d_transform_correspondence.py
 
 #Step 3: run batch inference to refine the final outputs
-torchrun --nproc_per_node=8 --master-port $FREE_PORT Eval/freefine_batch_infer_3d_depth.py #3d
+torchrun --nproc_per_node=8 --master-port $FREE_PORT freefine_batch_infer_3d_depth.py 
